d0ca3147b6bc5e1269841f6b91404e2aa399330c
Nema <nematoad00@gmail.com>
2025-10-27 23:57:16 -0400

    added navigation header with buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     
+      <header className="header">
+        <nav className="nav">
+         
+          <div className="nav-left">
+            <button className="nav-button">Resumize</button>
+            <button className="nav-button">About</button>
+            <button className="nav-button">Builder</button>
+            <button className="nav-button">Analyzer</button>
+          </div>
+          
+          <div className="nav-right">
+            <button className="nav-button">Theme</button>
+            <button className="nav-button">Login</button>
+          </div>
+        </nav>
+      </header>
+
       <div className="container">
         <div className="hero">
           <h1 className="title">Resumize</h1>
